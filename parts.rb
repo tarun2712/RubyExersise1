d95d5a1bf3416564f8af18a3eeadbe21ba27c0b3
tarun2712 <tnallamothu@nisum.com>
2016-08-02 11:09:17 -0700

    anged version

--- parts.rb
+++ parts.rb
@@ -6,12 +6,12 @@ require 'json'
 myXML  = Crack::XML.parse(File.read("parts.xml"))
 myJSON = myXML.to_json
 puts myJSON
-File.write('/Users/NIS1576-mbp/Desktop/myfile.json', myJSON)
+File.write('myfile.json', myJSON)
 CSV.open("mycsv.csv", "w") do |csv|
-	JSON.parse(File.open("/Users/NIS1576-mbp/desktop/myfile.json").read).each do |hash|
+	JSON.parse(File.open("myfile.json").read).each do |hash|
 		
  			puts hash
- 			File.write('/Users/NIS1576-mbp/Desktop/mycsv.csv', hash)
+ 			File.write('mycsv.csv', hash)
   	end
  end
 
